0b86980072023bb119d02ce3df5bfb9c9e7fcf3a
ronal cucariano <ronalcucariano@gmail.com>
2024-02-22 00:46:19 -0500

    inicio campechefinal

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/campeche_final/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "productos";
+const CHARSET = "charset=utf8";
+const TITLE = "PRODUCTOS";
+const MONEDA = "COP";
+const CLIENT_ID = "";
+?>
